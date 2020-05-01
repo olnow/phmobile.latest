ed19105eab79aaf148d32a43977a9d297321a2d0
Oleg <onovikov@gmail.com>
2020-05-01 19:57:22 +0300

    add state of selected month and year

--- frontend/src/modules/CashStore/stateCashStore.js
+++ frontend/src/modules/CashStore/stateCashStore.js
@@ -4,5 +4,7 @@ export default {
   cashMonth: [],
   cashYear: [],
   cashYearDepart: [],
+  selectedMonth: localStorage.getItem('cashSelectedMonth'),
+  selectedYear: localStorage.getItem('cashSelectedYear'),
   errors: []
 }
