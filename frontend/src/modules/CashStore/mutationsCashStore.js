c497bc03b305d7cc2c3f4842a4433645ef9c9f2a
Oleg <onovikov@gmail.com>
2020-05-01 19:57:05 +0300

    add state of selected month and year

--- frontend/src/modules/CashStore/mutationsCashStore.js
+++ frontend/src/modules/CashStore/mutationsCashStore.js
@@ -3,18 +3,22 @@ import { CASH_MUTATIONS } from '../mutation-types'
 export default {
   [CASH_MUTATIONS.MUTATION_LOADPHONESCASH] (state, { data }) {
     state.phoneList = data
-    // console.log(state.phoneList)
   },
   [CASH_MUTATIONS.MUTATION_LOADCASHMONTH] (state, { data }) {
     state.cashMonth = data
-    // console.log(state.phoneList)
   },
   [CASH_MUTATIONS.MUTATION_LOADCASHYEAR] (state, { data }) {
     state.cashYear = data
-    // console.log(state.phoneList)
   },
   [CASH_MUTATIONS.MUTATION_LOADCASH_YEAR_DEPART] (state, { data }) {
     state.cashYearDepart = data
-    // console.log(state.phoneList)
+  },
+  [CASH_MUTATIONS.SET_SELECTED_MONTH] (state, data) {
+    state.selectedMonth = data
+    // console.log('mutation: ', data, state.selectedMonth)
+  },
+  [CASH_MUTATIONS.SET_SELECTED_YEAR] (state, data) {
+    state.selectedYear = data
+    // console.log('mutation: ', data, state.selectedYear)
   }
 }
