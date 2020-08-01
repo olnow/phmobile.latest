deddbcc9cee02d8d9b9b7d6e566c347d26f247ac
Oleg <onovikov@gmail.com>
2020-08-01 12:25:32 +0300

    change to spring annotation

--- phmobile/src/main/java/olnow/phmobile/ServiceDAO.java
+++ phmobile/src/main/java/olnow/phmobile/ServiceDAO.java
@@ -5,15 +5,91 @@ import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
 import javax.persistence.NoResultException;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import java.util.ArrayList;
 
-@Component
+@Repository
+@Qualifier("ServiceDAO")
+public class ServiceDAO extends RootDAOImpl<Service> implements IServiceDAO<Service>  {// extends RootDAOImpl<Service> {
+    Logger logger = LoggerFactory.getLogger(ServiceDAO.class);
+
+    ServiceDAO() {
+        super(Service.class);
+    }
+
+    public Service find(Phones phones, String name, String code) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        CriteriaBuilder criteria = session.getCriteriaBuilder();
+        CriteriaQuery<Service> criteriaQuery = criteria.createQuery(Service.class);
+        Root<Service> root = criteriaQuery.from(Service.class);
+
+        criteriaQuery.select(root)
+                .where(criteria.and(
+                    criteria.equal(root.get(Service_.phone), phones),
+                    criteria.equal(root.get(Service_.name), name),
+                    criteria.equal(root.get(Service_.code), code)));
+        criteriaQuery.select(root);
+
+        Query<Service> query = session.createQuery(criteriaQuery);
+        try {
+            Service obj = query.getSingleResult();
+            if (obj != null) return obj;
+            else return null;
+        }
+        catch (NoResultException e) {
+            return null;
+        }
+        catch (HibernateException e) {
+            logger.error("[find] Hibernate exception, phone: {}, name: {}, code{}", phones, name, code, e);
+            throw e;
+        }
+        finally {
+            session.close();
+        }
+    }
+
+    public ArrayList<Service> get(Phones phones) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        CriteriaBuilder criteria = session.getCriteriaBuilder();
+        CriteriaQuery<Service> criteriaQuery = criteria.createQuery(Service.class);
+        Root<Service> root = criteriaQuery.from(Service.class);
+
+        criteriaQuery.select(root)
+                .where(criteria.equal(root.get(Service_.phone), phones));
+        criteriaQuery.select(root);
+
+        Query<Service> query = session.createQuery(criteriaQuery);
+        try {
+            ArrayList<Service> obj = (ArrayList<Service>) query.getResultList();
+            if (obj != null) return obj;
+            else return null;
+        }
+        catch (NoResultException e) {
+            return null;
+        }
+        catch (HibernateException e) {
+            logger.error("[get] Hibernate exception, phone: {}, name: {}, code{}", phones, e);
+            throw e;
+        }
+        finally {
+            session.close();
+        }
+
+    }
+
+    @Override
+    public ArrayList<Service> get() {
+        return get();
+    }
+}
+
+/*
+>>>>>>> Stashed changes
 public class ServiceDAO extends RootDAO<Service> {
     Logger logger = LoggerFactory.getLogger(ServiceDAO.class);
 
@@ -82,3 +158,4 @@ public class ServiceDAO extends RootDAO<Service> {
         return get("service");
     }
 }
+ */
