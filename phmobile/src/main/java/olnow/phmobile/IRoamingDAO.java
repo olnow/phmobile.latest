dfd7ddd2c2e1dd8e9d02c785b4c009255af999d2
Oleg <onovikov@gmail.com>
2020-08-12 21:01:34 +0300

    add interface IRoamingDAO

---
+++ phmobile/src/main/java/olnow/phmobile/IRoamingDAO.java
@@ -0,0 +1,7 @@
+package olnow.phmobile;
+
+// import javax.persistence.metamodel.SingularAttribute;
+
+public interface IRoamingDAO<T> extends IRootDAO<T> {
+    // Object findRoaming(SingularAttribute attr, String name);
+}
