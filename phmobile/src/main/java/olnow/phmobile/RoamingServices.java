9b90732cbd2c682feaf901827bc1314bebc56951
Oleg <onovikov@gmail.com>
2020-08-12 21:07:13 +0300

    change to spring annotation, use RootServiceImpl and IRoamingServices

--- phmobile/src/main/java/olnow/phmobile/RoamingServices.java
+++ phmobile/src/main/java/olnow/phmobile/RoamingServices.java
@@ -1,10 +1,20 @@
 package olnow.phmobile;
 
-import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
 
-public class RoamingServices {
-    private RoamingDAO roamingDAO = new RoamingDAO();
+@Repository
+@Qualifier("RoamingServices")
+public class RoamingServices extends RootServiceImpl<Roaming> implements IRoamingServices {
 
+    @Autowired
+    RoamingServices(IRoamingDAO roamingDAO) {
+        super(roamingDAO);
+    }
+    // private RoamingDAO roamingDAO = new RoamingDAO();
+
+    /*
     public void addRoaming(Roaming roaming) {
         roamingDAO.addRoaming(roaming);
     }
@@ -23,15 +33,15 @@ public class RoamingServices {
 
     public ArrayList<Roaming> getRoaming() {
         return roamingDAO.getRoaming();
-    }
+    }*/
 
     public Roaming findOrCreateRoaming(String roamingName) {
         if (roamingName == null || roamingName.isEmpty())
             return null;
-        Roaming roaming = findRoaming(roamingName);
+        Roaming roaming = find(Roaming_.roaming, roamingName);
         if (roaming == null) {
             roaming = new Roaming(roamingName);
-            roamingDAO.addRoaming(roaming);
+            rootDAO.add(roaming);
         }
         return roaming;
     }
