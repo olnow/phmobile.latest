6d57627bc2f648dcc0dd0f1fc8e73f80ca5c8169
Oleg <onovikov@gmail.com>
2020-08-12 21:04:55 +0300

    add method addService

--- phmobile/src/main/java/olnow/phmobile/IServiceServices.java
+++ phmobile/src/main/java/olnow/phmobile/IServiceServices.java
@@ -1,8 +1,13 @@
 package olnow.phmobile;
 
+
+import javassist.NotFoundException;
+
 import java.util.ArrayList;
+import java.util.Map;
 
 public interface IServiceServices extends IRootService<Service> {
     Service find(Phones phones, String name, String code);
     ArrayList<Service> get(Phones phones);
+    void addService(Map<String, String> maps) throws NotFoundException;
 }
