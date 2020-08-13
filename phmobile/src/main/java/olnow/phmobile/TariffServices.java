53184fa12fae3d82fd5563736af9ad449838fe1b
Oleg <onovikov@gmail.com>
2020-08-13 21:13:28 +0300

    add import

--- phmobile/src/main/java/olnow/phmobile/TariffServices.java
+++ phmobile/src/main/java/olnow/phmobile/TariffServices.java
@@ -1,5 +1,6 @@
 package olnow.phmobile;
 
+import olnow.phmobile.dao.TariffDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
